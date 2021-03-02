1d1cd97ddc1b981344fef25055b89c5b3e88f4e6
Trevor Jacob Bickmore <b1ckmor3@falcon.cs.byu.edu>
2021-03-02 14:01:37 -0700

    Just a test

--- ScriptRunner/ui/screen.py
+++ ScriptRunner/ui/screen.py
@@ -3,6 +3,7 @@ import curses
 class Screen:
     def __init__(self, parentScreen, title, folder):
         self.folder = folder
+        #adding this to see if it gits changed
         self.rows, self.columns = parentScreen.getmaxyx()
         if self.rows < len(self.folder._all_contents):
             self.rows = self.folder._all_contents
